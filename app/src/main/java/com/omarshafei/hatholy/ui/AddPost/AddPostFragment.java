73fe857cba1dbb54c813ccb9cac8ccb69c6f5c1d
Omar Shafei <omarshafhy.os@gmail.com>
2020-09-07 17:10:11 +0200

    Upload the image to Firebase Storage

--- app/src/main/java/com/omarshafei/hatholy/ui/AddPost/AddPostFragment.java
+++ app/src/main/java/com/omarshafei/hatholy/ui/AddPost/AddPostFragment.java
@@ -1,21 +1,27 @@
 package com.omarshafei.hatholy.ui.AddPost;
 
 import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.MimeTypeMap;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -23,13 +29,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.omarshafei.hatholy.MainActivity;
 import com.omarshafei.hatholy.R;
 import com.omarshafei.hatholy.ui.Search.Post;
 import com.squareup.picasso.Picasso;
 
+import java.util.Objects;
+import java.util.UUID;
+
 import static android.app.Activity.RESULT_OK;
+import static androidx.core.content.ContextCompat.getSystemService;
 
 public class AddPostFragment extends Fragment implements AdapterView.OnItemSelectedListener{
 
@@ -41,8 +61,11 @@ public class AddPostFragment extends Fragment implements AdapterView.OnItemSelec
     private ImageView missingImage;
     private EditText phoneNumber;
     private Button addPostButton;
+    private ProgressBar mProgressBar;
     private Uri mImageUri;
     private CollectionReference postsRef;
+    private StorageReference storageRef;
+    private StorageTask uploadTask;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -55,7 +78,10 @@ public class AddPostFragment extends Fragment implements AdapterView.OnItemSelec
         missingImage    = root.findViewById(R.id.missing_image_view);
         phoneNumber     = root.findViewById(R.id.number_edit_text);
         addPostButton   = root.findViewById(R.id.add_post_button);
+        mProgressBar = root.findViewById(R.id.progress_bar);
+
         postsRef = FirebaseFirestore.getInstance().collection("Posts");
+        storageRef = FirebaseStorage.getInstance().getReference();
         //fill the spinner with the data
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(requireContext(),
                 R.array.missing, R.layout.spinner_text);
@@ -88,14 +114,55 @@ public class AddPostFragment extends Fragment implements AdapterView.OnItemSelec
     }
 
     private void addPost() {
-        String number = phoneNumber.getText().toString();
-        String missingType = spinner.getSelectedItem().toString();
-        if(isValidPhoneNumber(number)) {
-            postsRef.add(new Post(number, missingType));
-            Toast.makeText(getContext(), "Post added", Toast.LENGTH_SHORT).show();
+        final String number = phoneNumber.getText().toString();
+        final String missingType = spinner.getSelectedItem().toString();
+        if(isValidPhoneNumber(number) && mImageUri != null) {
+
+            // Defining the child of storageReference
+            final StorageReference ref = storageRef.child("images/" + UUID.randomUUID().toString());
+
+            ref.putFile(mImageUri).addOnSuccessListener(
+                            new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+                                {
+                                    Handler handler = new Handler();
+                                    handler.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            mProgressBar.setProgress(0);
+                                        }
+                                    }, 500);
+                                    // Image uploaded successfully
+                                    ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                        @Override
+                                        public void onSuccess(Uri uri) {
+                                            Toast.makeText(getContext(), "Image Uploaded!", Toast.LENGTH_SHORT).show();
+                                            String imageUrl = uri.toString();
+                                            postsRef.add(new Post(number, missingType, imageUrl));
+                                        }
+                                    });
+                                }
+                            })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e)
+                        {
+                            // Error, Image not uploaded
+                            Toast.makeText(getContext(), "Failed " + e.getMessage(),
+                                            Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                    mProgressBar.setProgress((int) progress);
+                }
+            });
+        } else {
+            Toast.makeText(getContext(), "No file selected", Toast.LENGTH_SHORT).show();
         }
     }
-
     private boolean isValidPhoneNumber(String number) {
         boolean isValid = true;
         if(number.trim().length() == 11) {
