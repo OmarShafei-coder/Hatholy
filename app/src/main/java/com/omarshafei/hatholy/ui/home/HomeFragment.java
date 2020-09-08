512df93381cd036ea66a05f5dde43682355f4856
Omar Shafei <omarshafhy.os@gmail.com>
2020-09-08 21:06:50 +0200

    implement the Home fragment

--- app/src/main/java/com/omarshafei/hatholy/ui/home/HomeFragment.java
+++ app/src/main/java/com/omarshafei/hatholy/ui/home/HomeFragment.java
@@ -9,17 +9,35 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.omarshafei.hatholy.R;
 
 public class HomeFragment extends Fragment {
 
+    private TextView textView;
+    private TextView textView2;
+    private BottomNavigationView mBottomNavigationView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-
+        textView = root.findViewById(R.id.textView);
+        textView2 = root.findViewById(R.id.textView2);
+        mBottomNavigationView = requireActivity().findViewById(R.id.nav_view);
+        //open the fragment programmatically
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mBottomNavigationView.setSelectedItemId(R.id.navigation_add_post);
+            }
+        });
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mBottomNavigationView.setSelectedItemId(R.id.navigation_search);
+            }
+        });
         return root;
     }
 }
